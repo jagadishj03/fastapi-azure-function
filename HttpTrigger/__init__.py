9394ec626a5cff2b0d0256010256a9fcfd83dd08
jagadishj03 <jagadish.j03@gmail.com>
2025-07-28 01:01:10 +0530

    Changed entry point

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -1,5 +1,4 @@
 import azure.functions as func
-from fastapi.middleware.wsgi import WSGIMiddleware
 from .api import app
 
-main = func.AsgiFunctionApp(app=WSGIMiddleware(app))
+main = func.AsgiFunctionApp(app=app)
