9532a2f194e40ead46af03f0e8cd7cf8f42b50a1
jagadishj03 <jagadish.j03@gmail.com>
2025-07-28 00:30:57 +0530

    Added API

--- function_app.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-from fastapi import Request
-from app.main import app
-from mangum import Mangum
-
-handler = Mangum(app)
-
-async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return await handler(req.scope, req.body())
